ec9d418e9acdcd3e208bd0d22da2d7cc57255c09
Oleksandr Volskyi <136353538+OleksandrVolsky@users.noreply.github.com>
2026-02-25 11:55:07 +0200

    first js commit

---
+++ js/sort.js
@@ -0,0 +1,26 @@
+let block_sort = document.getElementById("block_sort");
+let sort_btn = document.getElementById("sort_btn");
+let reset_btn = document.getElementById("reset_btn");
+
+let arr = [];
+const n = 40;
+
+// функція створює елементи div 
+function createEl() {
+
+}
+
+// функція сортує елементи div 
+function bubbleSort() {
+
+}
+
+// кнопка ресет
+function resBtn() {
+
+}
+
+
+
+sort_btn.addEventListener("click", bubbleSort);
+reset_btn.addEventListener("click", resBtn);
